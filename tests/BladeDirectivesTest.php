4de2f33ccc34627ae3ea7c5c8c974d1046e012fb
John Koster <john@stillat.com>
2022-11-18 15:29:50 -0600

    Improves directive detection #5

--- tests/BladeDirectivesTest.php
+++ tests/BladeDirectivesTest.php
@@ -2,24 +2,21 @@
 
 namespace Stillat\BladeParser\Tests;
 
-use Stillat\BladeParser\Nodes\DirectiveNode;
 use Stillat\BladeParser\Nodes\StaticNode;
 use Stillat\BladeParser\Parsers\Blade;
 
 class BladeDirectivesTest extends ParserTestCase
 {
-    public function testDirectiveParserDoesNotEnterInfiniteLoop()
+    public function testDirectivesWithoutLeadingSpaceAreIgnored()
     {
         $template = '<input type="text" id="emailBackdrop" class="form-control" placeholder="xxxx@xxx.xx">';
         $parser = new Blade();
         $result = $parser->parse($template);
         $nodes = $result->getNodes();
 
-        $this->assertCount(3, $nodes);
+        $this->assertCount(1, $nodes);
         $this->assertInstanceOf(StaticNode::class, $nodes[0]);
-        $this->assertInstanceOf(DirectiveNode::class, $nodes[1]);
-        $this->assertInstanceOf(StaticNode::class, $nodes[2]);
 
-        $this->assertSame('@xxx', $nodes[1]->rawContent);
+        $this->assertSame('<input type="text" id="emailBackdrop" class="form-control" placeholder="xxxx@xxx.xx">', $nodes[0]->rawContent);
     }
 }
